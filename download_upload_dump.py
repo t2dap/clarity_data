9ed91304862b599c519f74b57761f74600e3ffa4
Alice_Sart <sartoria1@nychhc.org>
2022-03-02 16:08:19 -0500

    created one file only

--- download_upload_dump.py
+++ download_upload_dump.py
@@ -1,6 +1,6 @@
 import bcp
 import datetime
-from config import server_info, server_info_dev, database_testing, database_upload,load_table
+from config import server_info, server_info_dev, database_testing, first_query, database_upload,load_table
 import warnings
 import logging
 import time
@@ -13,16 +13,25 @@ import pyodbc
 
 from pathlib import Path
 
+from config import server_info, server_info_dev, database_testing, database_upload, load_table
+
+import multiprocessing
+# print(multiprocessing.cpu_count())
+
+from pathlib import Path
+
 # Disable the warnings
 warnings.filterwarnings('ignore')
 
-path_raw_files = '.\\daily_dump\\'
+path_raw_files = '.\\raw_files\\'
 # path_raw_files=r'C:\Users\sartoria1\Desktop\prova\raw_files'
 # print()
 today = datetime.datetime.now().strftime('%Y-%m-%d')
-filename = f'daily_dump_{today}'
+filename = f'dump_{today}'
 # filename = f'dump_{time}'
-# connect to the database Clarity
+
+
+# connect to the database Clarity and download the data dump from the assigned table
 def download_clarity(path_raw_files,filename, database):
 
     conn = bcp.Connection(host= server_info, driver='mssql')
@@ -63,13 +72,11 @@ def download_clarity(path_raw_files,filename, database):
                 ,admit_dep_name
                 ,Borough
                 ,Neighborhood
-        FROM [CovResponse].[Tableau].[V_COVID19_PCR_Testing_All_Care_Settings]
-        where ORDER_TIME > DATEADD(day,-1, GETDATE())''', output_file=file)
-    
+        FROM [CovResponse].[Tableau].[V_COVID19_PCR_Testing_All_Care_Settings]''', output_file=file)
 
-''' Load extracted data tp table the the specific view in CERT '''
 
-# connect to the database T2DAP
+'''create a connection to the database T2DAP'''
+
 def create_server_connection(server, database):
     conn = None
     try:
@@ -86,6 +93,9 @@ def create_server_connection(server, database):
 
     return conn
 
+    
+''' Load extracted data tp table the the specific view in CERT '''
+
 def load_data(cursor, df, table):
 
     # Insert DataFrame to Table
@@ -98,28 +108,45 @@ def load_data(cursor, df, table):
     cursor.commit()
 
 
+    # for row in df.itertuples():
+    #     cursor.execute('''
+    #                 INSERT INTO products (product_id, product_name, price)
+    #                 VALUES (?,?,?)
+    #                 ''',
+    #                 row.product_id, 
+    #                 row.product_name,
+    #                 row.price
+    #                 )
+    # conn.commit()
+
 #entrypoint
+
 if __name__ == '__main__':
+
+
+    '''run function to download bulk data from ophw database'''
 
     start = time.time()
     download_clarity(path_raw_files=path_raw_files,filename=filename, database=database_testing)
     stop = time.time()
     print(stop - start)
 
+    '''run function to upload bulk data to T2DAP database'''
+    
     columns = ['loc_name', 'facility_id', 'department_name', 'department_id','hospital_service', 'pat_id', 'pat_name', 'mrn','contact_date', 
-            'birth_date','order_time', 'ord_result_time', 'order_procedure_id','specimen_type', 'procedure_name', 'covid19_proc_type', 'age', 'age_group',
-            'patient_sex', 'patient_race', 'patient_language', 'result',
-            'zipcode','hosp_adm_time', 'inpatient_adm_date', 'hosp_disch_time',
-            'death_date', 'discharge_disposition',  'site_type',
-            'primary_payer_group', 'admit_dep_name', 
-            'borough', 'neighborhood']
+                'birth_date','order_time', 'ord_result_time', 'order_procedure_id','specimen_type', 'procedure_name', 'covid19_proc_type', 'age', 'age_group',
+                'patient_sex', 'patient_race', 'patient_language', 'result',
+                'zipcode','hosp_adm_time', 'inpatient_adm_date', 'hosp_disch_time',
+                'death_date', 'discharge_disposition',  'site_type',
+                'primary_payer_group', 'admit_dep_name', 
+                'borough', 'neighborhood']
 
     connection_to_load_tdap = create_server_connection(server=server_info_dev, database=database_upload)
     cursor=connection_to_load_tdap.cursor()
     # start = time.time()
+    filename = f'dump_{today}'
     path_filename = f'{path_raw_files}{filename}.csv'
-    print(path_filename)
-    chuncks = pd.read_csv(path_filename, delimiter='|' , dtype={"loc_name": str, "admit_dep_name":str ,"department_id":object ,"hospital_service":str}, names = columns, parse_dates=['birth_date','contact_date', 'ord_result_time','order_time','hosp_disch_time','inpatient_adm_date','hosp_adm_time','death_date'], chunksize= 10000)
+    chuncks = pd.read_csv(path_filename, delimiter='|' , dtype={"loc_name": str, "admit_dep_name":str ,"department_id":object ,"hospital_service":str}, names = columns, skiprows = 1000000, parse_dates=['birth_date','contact_date', 'ord_result_time','order_time','hosp_disch_time','inpatient_adm_date','hosp_adm_time','death_date'], chunksize=100000)
     # df= pd.read_csv(path_filename, delimiter='|' , dtype={"loc_name": str, "admit_dep_name":str ,"department_id":object ,"hospital_service":str}, names = columns, nrows=50, parse_dates=['birth_date','contact_date', 'ord_result_time','order_time','hosp_disch_time','inpatient_adm_date','hosp_adm_time','death_date'])
     # ,  dtype={"salary": int}
 
@@ -129,9 +156,8 @@ if __name__ == '__main__':
         # print(df['order_procedure_id'])
         start = time.time()
         
-        
-        df = df.astype(object).where(pd.notnull(df), None)
 
+        df = df.astype(object).where(pd.notnull(df), None)
         df['crtd_dt'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         # print(df.info())
         # df = df.fillna("NULL")
@@ -139,14 +165,8 @@ if __name__ == '__main__':
         # print(df['death_date'])
         # print(df['pat_name'])
         print(ii)
-       
+        # print(df)
         load_data(cursor,df,table=load_table)
     
         stop = time.time()
         print('Time elapsed: ', stop - start)
-    #     # print(df['mrn'].str.len().max())
-    #     # break
-    #     # if(ii == 5):
-    #     #     break
-    # cursor.close()
-    # connection_to_load_tdap.close()
