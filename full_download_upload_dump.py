5cd0ef08aa80cc02f3ca31d3503522b777b2844b
Sartori <A-sartoria1@corp.nychhc.org>
2022-03-03 11:15:52 -0500

    modifiedfull dump from vm

---
+++ full_download_upload_dump.py
@@ -0,0 +1,163 @@
+import bcp
+import datetime
+from config import server_info, server_info_dev, database_testing, database_upload,load_table
+import warnings
+import logging
+import time
+
+import chunk
+import time
+import pandas as pd
+
+import pyodbc
+
+from pathlib import Path
+
+from config import server_info, server_info_dev, database_testing, database_upload, load_table
+
+import multiprocessing
+# print(multiprocessing.cpu_count())
+
+from pathlib import Path
+
+# Disable the warnings
+warnings.filterwarnings('ignore')
+
+path_raw_files = '.\\raw_files\\'
+# path_raw_files=r'C:\Users\sartoria1\Desktop\prova\raw_files'
+# print()
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+filename = f'dump_{today}'
+# filename = f'dump_{time}'
+
+
+# connect to the database Clarity and download the data dump from the assigned table
+def download_clarity(path_raw_files,filename, database):
+
+    conn = bcp.Connection(host= server_info, driver='mssql')
+    my_bcp = bcp.BCP(conn)
+
+    path = Path(f'{path_raw_files}{filename}.csv')
+    file = bcp.DataFile(file_path= path, delimiter='|')
+    my_bcp.dump(query= f'''SELECT LOC_NAME
+                ,FACILITY_ID
+                ,DEPARTMENT_NAME
+                ,DEPARTMENT_ID
+                ,HOSPITAL_SERVICE
+                ,PAT_ID
+                ,PAT_NAME
+                , MRN
+                ,CONTACT_DATE
+                ,BIRTH_DATE
+                ,ORDER_TIME
+                ,ORD_RESULT_TIME
+                , ORDER_PROC_ID
+                ,SPECIMEN_TYPE
+                , PROC_NAME
+                , COVID19_PROC_TYPE
+                , AGE
+                , AGE_GROUP
+                ,PATIENT_SEX
+                ,PATIENT_RACE
+                ,PATIENT_LANGUAGE
+                ,RESULT
+                ,ZIP_CODE
+                ,HOSP_ADMSN_TIME
+                ,INP_ADM_DATE
+                ,HOSP_DISCH_TIME
+                ,Death_Date
+                ,Disch_Disposition
+                ,Site_Type
+                ,PRIMARY_PAYER_GROUP
+                ,admit_dep_name
+                ,Borough
+                ,Neighborhood
+        FROM [CovResponse].[Tableau].[V_COVID19_PCR_Testing_All_Care_Settings]''', output_file=file)
+
+
+'''create a connection to the database T2DAP'''
+
+def create_server_connection(server, database):
+    conn = None
+    try:
+        conn = pyodbc.connect(
+                Trusted_Connection='Yes',
+                Driver='{ODBC Driver 17 for SQL Server}',
+                Server= server,
+                Database= database
+            )
+        print('T2DAP SQL Server Database connection successful')
+
+    except pyodbc.Error as e:
+        print(f'Error: {e}')
+
+    return conn
+
+
+''' Load extracted data tp table the the specific view in CERT '''
+
+def load_data(cursor, df, table):
+
+    # Insert DataFrame to Table
+    print('Download Clarity')
+    insert_to_tmp_tbl=f"INSERT INTO {table} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+    cursor.fast_executemany = True
+    cursor.executemany(insert_to_tmp_tbl, df.values.tolist())
+    print(f'{len(df)} rows inserted to the {table} table')
+
+    cursor.commit()
+
+
+
+
+if __name__ == '__main__':
+
+
+    '''run function to download bulk data from ophw database'''
+
+    start = time.time()
+    download_clarity(path_raw_files=path_raw_files,filename=filename, database=database_testing)
+    stop = time.time()
+    print(stop - start)
+
+    '''run function to upload bulk data to T2DAP database'''
+
+    columns = ['loc_name', 'facility_id', 'department_name', 'department_id','hospital_service', 'pat_id', 'pat_name', 'mrn','contact_date',
+                'birth_date','order_time', 'ord_result_time', 'order_procedure_id','specimen_type', 'procedure_name', 'covid19_proc_type', 'age', 'age_group',
+                'patient_sex', 'patient_race', 'patient_language', 'result',
+                'zipcode','hosp_adm_time', 'inpatient_adm_date', 'hosp_disch_time',
+                'death_date', 'discharge_disposition',  'site_type',
+                'primary_payer_group', 'admit_dep_name',
+                'borough', 'neighborhood']
+
+    connection_to_load_tdap = create_server_connection(server=server_info_dev, database=database_upload)
+    cursor=connection_to_load_tdap.cursor()
+    # start = time.time()
+    filename = f'dump_{today}'
+    path_filename = f'{path_raw_files}{filename}.csv'
+    chunck_size_value = 100000
+    columns_date = ['birth_date','contact_date', 'ord_result_time','order_time','hosp_disch_time','inpatient_adm_date','hosp_adm_time','death_date']
+    chuncks = pd.read_csv(path_filename, delimiter='|', engine='python',
+                        dtype={"loc_name": str, "admit_dep_name":str ,"department_id":object ,"hospital_service":str},
+                        names = columns,
+                        parse_dates= columns_date,
+                        chunksize= chunck_size_value)
+
+
+    print('UPLOAD PART')
+    for ii, df in enumerate(chuncks):
+        # print(df['order_procedure_id'])
+        start = time.time()
+
+
+        df = df.astype(object).where(pd.notnull(df), None)
+        df['crtd_dt'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        print(ii)
+        print(df)
+        # print(df.loc[25085])
+        load_data(cursor,df,table=load_table)
+
+        stop = time.time()
+        print('Time elapsed: ', stop - start)
